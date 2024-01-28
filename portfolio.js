b4bfbe48e6a8e94987ddb15e3fc741c076032394
Robyn H Richardson <robynhrich@icloud.com>
2024-01-27 23:07:17 -0500

    Linked jQuery to all pages

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,18 @@
+$("#about-me-button").click(function () {
+  document.location.href =
+    "https://robynhrich.netlify.app/#aboutme" + $(this).attr("id");
+});
+
+$("#contact-button").click(function () {
+  document.location.href =
+    "https://robynhrich.netlify.app/contact.html" + $(this).attr("id");
+});
+
+$("#portfolio-button").click(function () {
+  document.location.href =
+    "https://robynhrich.netlify.app/portfolio.html" + $(this).attr("id");
+});
+
 $("#email-button").click(function () {
   document.location.href = "mailto:robynhrich@icloud.com" + $(this).attr("id");
 });
